bf450f69d63eb8ad260a4e34884f7c2a69d92274
Afoghandi <ralphaeloshun@gmail.com>
2022-10-22 00:20:25 +0100

    user edit column

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -5,6 +5,7 @@ import {
 	PhoneAndroid,
 	MailOutline,
 	LocationSearching,
+	Publish,
 } from '@material-ui/icons';
 
 export default function User() {
@@ -52,7 +53,67 @@ export default function User() {
 						</div>
 					</div>
 				</div>
-				<div className='userUpdate'></div>
+				<div className='userUpdate'>
+					<span className='userUpdateTitle'>Edit</span>
+					<form action='' className='userUpdateForm'>
+						<div className='userUpdateLeft'>
+							<div className='userUpdateItem'>
+								<label>Username</label>
+								<input
+									type='text'
+									placeholder='arrrya999'
+									className='userUpdateInput'
+								/>
+							</div>
+							<div className='userUpdateItem'>
+								<label>Full Name</label>
+								<input
+									type='text'
+									placeholder='Arya Stark'
+									className='userUpdateInput'
+								/>
+							</div>
+							<div className='userUpdateItem'>
+								<label>Email</label>
+								<input
+									type='text'
+									placeholder='arya999@gmail.com'
+									className='userUpdateInput'
+								/>
+							</div>
+							<div className='userUpdateItem'>
+								<label>Phone</label>
+								<input
+									type='text'
+									placeholder='+44 0906554 567'
+									className='userUpdateInput'
+								/>
+							</div>
+							<div className='userUpdateItem'>
+								<label>Address</label>
+								<input
+									type='text'
+									placeholder='London | England'
+									className='userUpdateInput'
+								/>
+							</div>
+						</div>
+						<div className='userUpdateRight'>
+							<div className='userUpdateUpload'>
+								<img
+									src='https://images.pexels.com/photos/2065203/pexels-photo-2065203.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+									alt=''
+									className='userUpdateImg'
+								/>
+								<label htmlFor='file'>
+									<Publish />{' '}
+								</label>
+								<input type='file' id='file' style={{ display: 'none' }} />
+							</div>
+							<button className='userUpdateButton'>Update</button>
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
 	);
